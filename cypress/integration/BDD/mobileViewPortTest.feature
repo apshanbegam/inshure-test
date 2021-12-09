acfe7f72c8787c3e7d41bafc97151f9803b895b0
apshanbegam <apshanrahuman31@gmail.com>
2021-12-09 01:19:58 +0000

    mobile test

---
+++ cypress/integration/BDD/mobileViewPortTest.feature
@@ -0,0 +1,10 @@
+Feature: Set Mobile Viewport and verify the brand logo is visible
+
+    This would set Mobile Viewport and verify the brand logo is visible
+
+    Scenario: Mobile Viewport and verify brand logo
+
+        Given The user visits Inshur Test Website Via Mobile
+        
+        Then Brand Logo is visible
+
---
+++ cypress/integration/BDD/mobileViewPortTest/mobileViewPortTestStepDef.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+import { Given,When,Then, And } from "cypress-cucumber-preprocessor/steps"; 
+
+Given("The user visits Inshur Test Website Via Mobile", function(){
+    cy.viewport(320, 480); 
+    cy.visit("http://localhost:3000").contains("Cypress Test");
+})
+
+Then("Brand Logo is visible",()=>{
+    cy.get('#root > div > header > img').should('have.attr', 'src', '/static/media/logo.4feb383e.png')
+})
